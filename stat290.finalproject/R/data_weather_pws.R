09fff0a8ee9345db3ca4e9657b559465da9b726c
tianjiangw <47904198+tianjiangw@users.noreply.github.com>
2019-03-17 12:54:56 -0700

    project package updated

---
+++ stat290.finalproject/R/data_weather_pws.R
@@ -0,0 +1,11 @@
+#' weather_pws data for 290 final project
+#'
+#' @format a large list (3560 elements, 10.3Mb):
+#' \describe{
+#'   \item{}{}
+#'   \item{}{}
+#'   ...
+#' }
+#' @source \url{http://www.stanford.edu/}
+
+"weather_pws"
